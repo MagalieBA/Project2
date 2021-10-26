8bb26427c6a191716b2d8208a210b41a01d50aca
MagalieBA <magouilleba@gmail.com>
2021-10-27 00:11:37 +0200

    category extraction

--- Project2.py
+++ Project2.py
@@ -5,8 +5,6 @@ import csv
 from bs4 import BeautifulSoup
 
 website_base_url = "https://books.toscrape.com/"
-test_url = "https://books.toscrape.com/catalogue/set-me-free_988/index.html"
-
 
 def book_extraction(link_book):        # FUNCTION TO EXTRACT FROM A BOOK PAGE
     #PARSER
@@ -29,7 +27,6 @@ def book_extraction(link_book):        # FUNCTION TO EXTRACT FROM A BOOK PAGE
     image_tag = soup.find("img", alt=title).attrs["src"] #Extract the image url
     image_url = str(website_base_url) + str(image_tag[6:]) #Converts url link from relative to absolute 
 
-
     #Creating a dictionnary with the extracted informations of the book
     book_information = {
         "Page_url": product_page_url,
@@ -43,59 +40,49 @@ def book_extraction(link_book):        # FUNCTION TO EXTRACT FROM A BOOK PAGE
         "Rating": review_rating,
         "Image": image_url,
     }
-
-    print(book_information)
-
-"""
-    #Creer mon fichier CSV
-    with open ('Example.csv','w',newline = '') as csvfile: # Example.csv gets created in the current working directory
-        my_writer = csv.writer(csvfile, delimiter = ' ')
-        my_writer.writerows(book_information)
-    
-export csv file
-        with open ('Example.csv','w',newline = '') as csvfile: 
-            links_to_books = csv.writer(csvfile,delimiter = ' ')
-            links_to_books.writerows(all_books_urls)
-"""
+    return book_information
 
 #EXTRACTING ALLS BOOKS IN A CATEGORY
-#PARSER
-category_url = "https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html" #will become a variable
-category_page = requests.get(category_url)
-most_soup = BeautifulSoup(category_page.content, 'html.parser')
+def extract_books_url(input_category_url): 
 
-#BROWSING ALL THE PAGE
-total_pages = most_soup.find("li", class_="current").string
-max_pages = int(total_pages.split("")[-1])
-print(max_pages)
-pages = [2,3,4]
-all_pages_url = [category_url,]
+    #PARSER
+    category_page = requests.get(input_category_url)
+    most_soup = BeautifulSoup(category_page.content, 'html.parser')
 
+    #BROWSING ALL THE PAGE
+    total_pages = most_soup.find("li", class_="current").string
+    max_pages = int(total_pages.split()[-1]) + 1
+    all_pages_url = [input_category_url,] # wrong link
 
-for i in pages :
-    next_page_url = str(website_base_url) + str("page-") + str(i) + str(".html")
-    all_pages_url += [next_page_url]
+    for i in range(2,max_pages) :
+        next_page_url = str(input_category_url[:-11]) + str("page-") + str(i) + str(".html") #wrong link
+        all_pages_url += next_page_url
 
-#EXTRACTING ALL THE BOOKS URLS
-for page in all_pages_url :
-    #parser
-    current_page = requests.get(page)
-    more_soup = BeautifulSoup(current_page.content, 'html.parser')
+    #EXTRACTING ALL THE BOOKS URLS
+    for page in all_pages_url :
+        #parser
+        current_page = requests.get(page)
+        book_soup = BeautifulSoup(current_page.content, 'html.parser')
 
-    all_books_urls = []
-    all_books = more_soup.find_all("article", class_="product_pod")
+        all_books_urls = []
+        all_books = book_soup.find_all("article", class_="product_pod")
 
-    """
-    #for book in all_books:
-        #book_url_relative = book.find("h3").find("a").attrs["href"]
-        #book_url= str(website_base_url) + str(cat) + str(book_url_relative[9:]) #converts relative link to absolute
-        #all_books_urls.append(book_url)
-        #book_extraction(book_url)"""
+    #Creer mon fichier CSV
+    with open ('BookInformations.csv','w',newline = '') as csvfile: # Example.csv gets created in the current working directory
+        my_writer = csv.writer(csvfile, delimiter = ' ')
+        for book in all_books:
+            book_url_relative = book.find("h3").find("a").attrs["href"]
+            book_url= str(category_url[:-11]) + str(book_url_relative[9:]) #converts relative link to absolute 
+            all_books_urls.append(book_url)
+            book_info = book_extraction(book_url)
+            my_writer.writerows(book_info) #Test
 
 #EXTRACTING ALL THE CATEGORY URLS
-    #parser
-    website = requests.get(website_base_url)
-    most_soup = BeautifulSoup(website.content, 'html.parser')
+website = requests.get(website_base_url)
+category_soup = BeautifulSoup(website.content, 'html.parser')
 
-    all_categories_urls = []
-    all_categories = most_soup.find("ul", class_="nav")
+all_categories = category_soup.find("ul", class_="nav").find_all("a")
+for category in all_categories:
+    category_url_relative = category.attrs["href"]
+    category_url= str(website_base_url) + str(category_url_relative) #converts relative link to absolute 
+    extract_books_url(category_url)
